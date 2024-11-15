d4f34711fde372ed68cc0a41aab85bd3573c3598
Robert Cunha <robert.cunha@techgrowth-labs.com.br>
2024-11-15 19:32:06 -0300

    fix: schema factory

--- src/common/database/schema.factory.ts
+++ src/common/database/schema.factory.ts
@@ -1,9 +1,17 @@
 import mongoose from "mongoose";
+import { SchemaProp } from "./prop.decorator";
 
 export class SchemaFactory {
 	static createFromClass<T>(target: new () => T) {
-		const options = Reflect.getMetadata(`mongooose:schema:`, target);
-		const metadata = Reflect.getMetadata(target.name, target);
-		return new mongoose.Schema(metadata, options);
+		const options = Reflect.getMetadata(
+			`mongoose:schema:options:${target.name}`,
+			target
+		);
+		const keys = Object.keys(target);
+		const schemaKeys = keys.reduce<Record<string, SchemaProp>>((prev, key) => {
+			return (prev[key] = Reflect.getMetadata(`${target.name}:${key}`, target));
+		}, {});
+		console.log(schemaKeys);
+		return new mongoose.Schema(schemaKeys, options);
 	}
 }
