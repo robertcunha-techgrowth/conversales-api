fb0795de14547b407e34852f18ed350baf20ff45
Robert Cunha <robert.cunha@techgrowth-labs.com.br>
2024-11-18 17:19:07 -0300

    feat: add list products and selected products step

--- src/step/step.service.ts
+++ src/step/step.service.ts
@@ -2,86 +2,17 @@ import { Channel } from "../channel/channel";
 import { StatusTicket, Ticket } from "../ticket/ticket";
 import { ChatGptRoles } from "../chatbot/chatgpt/chatgpt";
 import { Step } from "./step.entity";
-import { ChatBot } from "../chatbot/chatbot";
+import { ChatBot, ChatBotResponse } from "../chatbot/chatbot";
 import { Model } from "mongoose";
 import { Inject } from "../common/dependency-injection/inject";
-import { ChatCompletionMessageParam } from "openai/resources";
+import { ProductService } from "../product/product.service";
 
-export interface StepService {
-	run(ticket: Ticket, contentFromWpp: string): Promise<void>;
-}
+export abstract class StepService {
+	constructor(private readonly model: Model<Step>) {}
 
-export class NormalStep implements StepService {
-	constructor(
-		// coesão foi pro caralho aqui
-		// toDo: arruma essa porra
-		// cohesion goin to hell here
-		// toDo: fix this
-		@Inject("StepModel") private readonly model: Model<Step>,
-		@Inject("Chatbot") private readonly chatbot: ChatBot,
-		@Inject("Channel") private readonly channel: Channel,
-		@Inject("TicketModel") private readonly ticketModel: Model<Ticket>,
-		@Inject("Prompt") private readonly prompt: string
-	) {}
+	abstract run(ticket: Ticket, contentFromWpp: string): Promise<void>;
 
-	private async startPrompt() {
-		const config = {
-			content: this.prompt,
-			role: ChatGptRoles.System,
-		};
-		const history: ChatCompletionMessageParam[] = [];
-		await this.chatbot.sendMessage(config.content, history);
-		return history;
-	}
-
-	async run(ticket: Ticket, contentFromWpp: string) {
-		console.log(ticket);
-		const step = await this.findStep(ticket.currentStep);
-
-		const history = await this.firstStep(step);
-
-		if (history) {
-			ticket.history = history;
-		}
-
-		const contentResponse = await this.chatbot.sendMessage(
-			JSON.stringify({
-				message: contentFromWpp,
-				rule: step.rule,
-			}),
-			ticket.history
-		);
-
-		const { statusCode } = JSON.parse(contentResponse);
-
-		console.log(statusCode);
-
-		await this.channel.sendMessage(
-			ticket.from,
-			step.texts[statusCode.toString() as "200" | "400"]
-		);
-
-		// toDo: return this to chatbot
-		// chat bot handle with the update
-		if (statusCode === 200) {
-			await this.ticketModel.findOneAndUpdate(
-				{
-					documentId: ticket.documentId,
-				},
-				{
-					step: ticket.isFinalStep
-						? ticket.currentStep
-						: ticket.currentStep + 1,
-					status: ticket.isFinalStep
-						? StatusTicket.Closed
-						: StatusTicket.Active,
-					history: ticket.history,
-				}
-			);
-		}
-	}
-
-	private async findStep(currentStep: number): Promise<Step> {
+	protected async findStep(currentStep: number): Promise<Step> {
 		const step = await this.model
 			.findOne<Step>({
 				stepNumber: currentStep,
@@ -98,70 +29,226 @@ export class NormalStep implements StepService {
 		}
 		return step;
 	}
+}
 
-	private async firstStep(step: Step) {
-		if (step.isFirstStep) {
-			const history = await this.startPrompt();
-			return history;
+export class ListProductsStep extends StepService {
+	constructor(
+		@Inject("StepModel") model: Model<Step>,
+		@Inject("Chatbot") private readonly chatbot: ChatBot,
+		@Inject("Channel") private readonly channel: Channel,
+		@Inject("ProductService") private readonly productService: ProductService,
+		@Inject("TicketModel") private readonly ticketModel: Model<Ticket>
+	) {
+		super(model);
+	}
+
+	async run(ticket: Ticket, contentFromWpp: string) {
+		const step = await this.findStep(ticket.currentStep);
+
+		const messageToSend = {
+			message: contentFromWpp,
+			rule: step.rule,
+		};
+
+		const stringfiedMessageToSend = JSON.stringify(messageToSend);
+
+		const contentResponse = await this.chatbot.sendMessage(
+			stringfiedMessageToSend
+		);
+
+		console.log(`Content response: ${(contentResponse as any).text.value}`);
+
+		ticket.history.push({
+			role: ChatGptRoles.User,
+			content: stringfiedMessageToSend,
+		});
+
+		const { statusCode, message } = JSON.parse(
+			(contentResponse as any).text.value
+		) as ChatBotResponse;
+
+		console.log(statusCode);
+		console.log(message);
+
+		ticket.history.push({
+			role: ChatGptRoles.Assistant,
+			content: JSON.stringify({
+				statusCode,
+				message,
+			}),
+		});
+
+		const products = await this.productService.findAll();
+		const productsText = products
+			.map((product) => {
+				return `${product.name} - ${product.price}\n${product.description}`;
+			})
+			.join("\n");
+		const stepBaseText = step.texts[statusCode.toString() as "200" | "400"];
+
+		await this.channel.sendMessage(
+			ticket.from,
+			`${stepBaseText}\n${productsText}`
+		);
+
+		if (statusCode === 200) {
+			await this.ticketModel.findOneAndUpdate(
+				{
+					_id: ticket._id,
+				},
+				{
+					currentStep: step.isFinalStep
+						? ticket.currentStep
+						: ticket.currentStep + 1,
+					status: step.isFinalStep ? StatusTicket.Closed : StatusTicket.Active,
+					history: ticket.history,
+				}
+			);
 		}
-		return null;
 	}
 }
 
-// export class ListProductsStep implements StepService {
-// 	constructor(
-// 		private readonly model: Model<Step>,
-// 		private readonly chatbot: ChatBot,
-// 		private readonly channel: WhatsapBusiness,
-// 		private readonly modelTicket: Model<Ticket>,
-// 		private readonly productModel: Model<Product>
-// 	) {}
+export class SelectedProductStep extends StepService {
+	constructor(
+		@Inject("StepModel") model: Model<Step>,
+		@Inject("Chatbot") private readonly chatbot: ChatBot,
+		@Inject("Channel") private readonly channel: Channel,
+		@Inject("ProductService") private readonly productService: ProductService,
+		@Inject("TicketModel") private readonly ticketModel: Model<Ticket>
+	) {
+		super(model);
+	}
 
-// 	async run(ticket: Ticket, contentFromWpp: string) {
-// 		const step = await this.model.findOne<Step>({
-// 			stepNumber: ticket.currentStep,
-// 		});
+	async run(ticket: Ticket, contentFromWpp: string): Promise<void> {
+		const step = await this.findStep(ticket.currentStep);
 
-// 		const { statusCode } = await this.chatbot.sendMessage(
-// 			JSON.stringify({
-// 				message: contentFromWpp,
-// 				rules: step.rules,
-// 			})
-// 		);
+		const messageToSend = {
+			message: contentFromWpp,
+			rule: step.rule,
+		};
 
-// 		const products = await this.productModel.find().lean();
+		const stringfiedMessageToSend = JSON.stringify(messageToSend);
 
-// 		if (statusCode === 200) {
-// 			await this.modelTicket.findOneAndUpdate(
-// 				{
-// 					documentId: ticket.documentId,
-// 				},
-// 				{
-// 					step: ticket.isFinalStep
-// 						? ticket.currentStep
-// 						: ticket.currentStep + 1,
-// 					status: ticket.isFinalStep
-// 						? StatusTicket.Closed
-// 						: StatusTicket.Active,
-// 				}
-// 			);
+		const contentResponse = await this.chatbot.sendMessage(
+			stringfiedMessageToSend
+		);
 
-// 			await this.channel.sendButtonMessage(
-// 				ticket.userPhone,
-// 				step.texts[statusCode],
-// 				products.map<WhatsappBusinessButton>((product) => ({
-// 					type: "product",
-// 					reply: {
-// 						id: product._id.toString(),
-// 						title: product.name,
-// 					},
-// 				}))
-// 			);
-// 		} else {
-// 			await this.channel.sendMessage(
-// 				ticket.userPhone,
-// 				step.texts[statusCode as 400]
-// 			);
-// 		}
-// 	}
-// }
+		console.log(`Content response: ${(contentResponse as any).text.value}`);
+
+		ticket.history.push({
+			role: ChatGptRoles.User,
+			content: stringfiedMessageToSend,
+		});
+
+		const { statusCode, message } = JSON.parse(
+			(contentResponse as any).text.value
+		) as ChatBotResponse;
+
+		console.log(statusCode);
+		console.log(message);
+
+		ticket.history.push({
+			role: ChatGptRoles.Assistant,
+			content: JSON.stringify({
+				statusCode,
+				message,
+			}),
+		});
+
+		const product = await this.productService.findOne({
+			filter: {
+				num: +message,
+			},
+		});
+
+		const stepBaseText = step.texts[statusCode.toString() as "200" | "400"];
+
+		const checkoutText = `Nome: ${product.name}\nPreço: ${product.price}\nDescrição: ${product.description}`;
+
+		await this.channel.sendMessage(
+			ticket.from,
+			`${stepBaseText}\n${checkoutText}`
+		);
+
+		if (statusCode === 200) {
+			await this.ticketModel.findOneAndUpdate(
+				{
+					_id: ticket._id,
+				},
+				{
+					currentStep: step.isFinalStep
+						? ticket.currentStep
+						: ticket.currentStep + 1,
+					status: step.isFinalStep ? StatusTicket.Closed : StatusTicket.Active,
+					history: ticket.history,
+				}
+			);
+		}
+	}
+}
+
+export class NormalStep extends StepService {
+	constructor(
+		// coesão foi pro caralho aqui
+		// toDo: arruma essa porra
+		// cohesion goin to hell here
+		// toDo: fix this
+		@Inject("StepModel") model: Model<Step>,
+		@Inject("Chatbot") private readonly chatbot: ChatBot,
+		@Inject("Channel") private readonly channel: Channel,
+		@Inject("TicketModel") private readonly ticketModel: Model<Ticket>
+	) {
+		super(model);
+	}
+
+	async run(ticket: Ticket, contentFromWpp: string) {
+		const step = await this.findStep(ticket.currentStep);
+
+		const messageToSend = {
+			message: contentFromWpp,
+			rule: step.rule,
+		};
+
+		const stringfiedMessageToSend = JSON.stringify(messageToSend);
+
+		const contentResponse = await this.chatbot.sendMessage(
+			stringfiedMessageToSend
+		);
+
+		console.log(`Content response: ${(contentResponse as any).text.value}`);
+
+		ticket.history.push({
+			role: ChatGptRoles.User,
+			content: stringfiedMessageToSend,
+		});
+
+		const { statusCode, message } = JSON.parse(
+			(contentResponse as any).text.value
+		) as ChatBotResponse;
+
+		console.log(statusCode);
+		console.log(message);
+
+		await this.channel.sendMessage(
+			ticket.from,
+			step.texts[statusCode.toString() as "200" | "400"]
+		);
+
+		// toDo: return this to chatbot
+		// chat bot handle with the update
+		if (statusCode === 200) {
+			await this.ticketModel.findOneAndUpdate(
+				{
+					_id: ticket._id,
+				},
+				{
+					currentStep: step.isFinalStep
+						? ticket.currentStep
+						: ticket.currentStep + 1,
+					status: step.isFinalStep ? StatusTicket.Closed : StatusTicket.Active,
+					history: ticket.history,
+				}
+			);
+		}
+	}
+}
