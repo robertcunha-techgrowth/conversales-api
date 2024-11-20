684fd3653162c8bbc1c7aa3442ee458c87e651a9
Robert Cunha <robert.cunha@techgrowth-labs.com.br>
2024-11-20 16:20:56 -0300

    feat: payment

---
+++ src/payment/payment.entity.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { SchemaFactory } from "../common/database/schema.factory";
+import { Ticket } from "../ticket/ticket";
+import { Prop } from "../common/database/prop.decorator";
+import { Schema } from "../common/database/schema.decorator";
+
+@Schema({
+	timestamps: true,
+})
+export class Payment {
+	@Prop({
+		type: mongoose.Types.ObjectId,
+		ref: Ticket.name,
+		required: true,
+	})
+	ticketId: mongoose.Types.ObjectId | Ticket;
+}
+
+export const PaymentSchema = SchemaFactory.createFromClass(Payment);
