34066218272147ffaed602323ec664ea33f73854
Robert Cunha <robert.cunha@techgrowth-labs.com.br>
2024-11-26 19:03:24 -0300

    refactor: change interface

--- src/chatbot/chatbot.ts
+++ src/chatbot/chatbot.ts
@@ -1,14 +1,32 @@
-import OpenAI from "openai";
+import { StepKind } from "../step/step.entity";
 
-export interface ChatBotResponse {
-	statusCode: number;
-	message?: string;
+export interface InputStepParams {}
+
+export interface OutputTaskInterpretation {
+	step: StepKind;
+	params?: InputStepParams;
+}
+
+export interface UserMessageParams {}
+export interface InterpretateMessageResponse {
+	step: string;
+	params?: UserMessageParams;
+}
+
+export interface GetMessageResponse {
+	message: string;
 }
 
 export interface ChatBotHistory {
 	role: string;
 	content: string;
 }
+
 export interface ChatBot {
-	sendMessage(message: string): Promise<string>;
+	getMessage(
+		inputParams: InputStepParams,
+		step: string
+	): Promise<GetMessageResponse>;
+
+	interpretateMessage(message: string): Promise<InterpretateMessageResponse>;
 }
