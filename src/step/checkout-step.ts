eab974010fe5b5da964776496196bb4061ae19c6
Robert Cunha <robert.cunha@techgrowth-labs.com.br>
2024-12-02 17:37:00 -0300

    fix: add constructor for checkout

--- src/step/checkout-step.ts
+++ src/step/checkout-step.ts
@@ -1,18 +1,30 @@
+import { Model } from "mongoose";
 import {
 	UserMessageParams,
 	OutputTaskInterpretation,
 } from "../chatbot/chatbot";
+import { Inject } from "../common/dependency-injection/inject";
 import { Injectable } from "../common/dependency-injection/injectable";
 import { Ticket } from "../ticket/ticket.entity";
-import { StepKind } from "./step.entity";
+import { Step } from "./step.entity";
 import { StepService } from "./step.service";
 
 @Injectable()
 export class CheckoutStep extends StepService {
+	constructor(
+		@Inject("StepModel") stepModel: Model<Step>,
+		@Inject("TicketModel") ticketModel: Model<Ticket>
+	) {
+		super(stepModel, ticketModel);
+	}
+
 	override async run(
 		ticket: Ticket,
 		_params?: UserMessageParams
 	): Promise<OutputTaskInterpretation> {
+		const step = await this.model.findOne({
+			stepNumber: ticket.currentStep,
+		});
 		const { user, cart } = ticket;
 		if (!cart.length) {
 			throw {
@@ -31,7 +43,7 @@ export class CheckoutStep extends StepService {
 			};
 		}
 		return {
-			step: StepKind.CHECKOUT,
+			rule: step.rule,
 			params: {
 				user: user,
 				products: cart.map((product) => {
