e6462f5c3f89a2e980b4c51b02b7f0cc84e66a81
Robert Cunha <robert.cunha@techgrowth-labs.com.br>
2024-12-02 19:02:23 -0300

    refactor: remove a lot of shit

--- src/step/step.entity.ts
+++ src/step/step.entity.ts
@@ -1,33 +1,15 @@
 import { Prop } from "../common/database/prop.decorator";
 import { Schema } from "../common/database/schema.decorator";
 import { SchemaFactory } from "../common/database/schema.factory";
-
-@Schema({
-	id: false,
-})
-export class StepTexts {
-	@Prop({
-		type: String,
-		required: true,
-	})
-	"200": string;
-
-	@Prop({
-		type: String,
-		required: true,
-	})
-	"400": string;
-}
-
-export const StepTextsSchema = SchemaFactory.createFromClass(StepTexts);
+import { Menu, MenuSchema } from "../menu/menu.entity";
 
 export enum StepKind {
-	COLLECT_DATA_STEP = "COLLECT_DATA_STEP",
 	GREETING = "GREETING",
 	LIST_PRODUCTS = "LIST_PRODUCTS",
 	ADD_PRODUCT = "ADD_PRODUCT",
 	CHECKOUT = "CHECKOUT",
 	SET_PROPERTY = "SET_PROPERTY",
+	DETECT_STEP = "DETECT_STEP",
 }
 @Schema({
 	timestamps: true,
@@ -46,52 +28,36 @@ export class Step {
 	rule: string;
 
 	@Prop({
-		type: StepTextsSchema, // Reference the schema here
-		required: true,
-	})
-	texts: StepTexts;
-
-	@Prop({
-		type: Boolean,
-		default: () => true,
-		required: true,
-	})
-	isFirstStep: boolean;
-
-	@Prop({
-		type: Boolean,
-		required: true,
-	})
-	isFinalStep: boolean;
-
-	@Prop({
-		type: Number,
-		default: null,
-	})
-	conditionalToFinish?: number;
-
-	@Prop({
-		type: String,
-	})
-	userProperty?: string;
-
-	@Prop({
-		type: Number,
-		default: null,
-	})
-	stepReturn?: number;
-
-	@Prop({
 		type: String,
 		required: true,
 	})
 	kind: StepKind;
 
 	@Prop({
+		type: MenuSchema,
+		required: false,
+	})
+	menu?: Menu;
+
+	@Prop({
+		type: Number,
+		required: false,
+		default: null,
+	})
+	detectStepToRedirect?: number;
+
+	@Prop({
+		type: String,
+		required: false,
+		default: null,
+	})
+	property?: string;
+
+	@Prop({
 		type: Number,
 		default: null,
 	})
-	nextStep?: number;
+	chainedStep?: number;
 }
 
 export const StepSchema = SchemaFactory.createFromClass(Step);
