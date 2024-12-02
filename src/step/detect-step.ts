a35a45dcd172a3819c96845566816b71eed6372d
Robert Cunha <robert.cunha@techgrowth-labs.com.br>
2024-12-02 18:58:37 -0300

    feat: detect step

---
+++ src/step/detect-step.ts
@@ -0,0 +1,64 @@
+// usuário manda mensagem - bot faz a introdução e exibe o menu - usuario escolhe uma opção
+// caminho de venda -> listar produtos -> usuário seleciona o produto -> adiciona produto ao carrinho -> pede nome -> usuário preenche nome
+// pede email -> usuário preenche email -> exibe o checkout para o usuário -> usuario confirma o checkout -> exibe o pagamento -> usuario paga
+// webhook dispara -> usuário recebe mensagem de confirmação de pagamento
+
+import { Model } from "mongoose";
+import { Inject } from "../common/dependency-injection/inject";
+import { Injectable } from "../common/dependency-injection/injectable";
+import { Ticket } from "../ticket/ticket.entity";
+import { AddProductStep } from "./add-product-step";
+import { CheckoutStep } from "./checkout-step";
+import { ListProductsStep } from "./list-product-step";
+import { SetPropertyStep } from "./set-property-step";
+import { Step, StepKind } from "./step.entity";
+import { StepService } from "./step.service";
+
+@Injectable()
+export class DetectStep extends StepService {
+	private options: Record<string, StepService> = {};
+
+	constructor(
+		@Inject("StepModel") model: Model<Step>,
+		@Inject("TicketModel") ticketModel: Model<Ticket>,
+		@Inject(ListProductsStep.name) listProductStep: StepService,
+		@Inject(AddProductStep.name) addProduct: StepService,
+		@Inject(CheckoutStep.name) checkout: StepService,
+		@Inject(SetPropertyStep.name) setProperty: StepService
+	) {
+		super(model, ticketModel);
+		this.options = {
+			[StepKind.LIST_PRODUCTS]: listProductStep,
+			[StepKind.ADD_PRODUCT]: addProduct,
+			[StepKind.CHECKOUT]: checkout,
+			[StepKind.SET_PROPERTY]: setProperty,
+		};
+	}
+
+	override async run(ticket: Ticket, text: string) {
+		const option = this.validateText(text);
+		const step = await this.model
+			.findOne({
+				stepNumber: ticket.currentStep,
+			})
+			.lean();
+		const menuItem = step.menu.items.find((item) => item.key === option);
+		const nextStepNumber = menuItem.stepNumber;
+		const nextStep = await this.model.findOne({
+			stepNumber: nextStepNumber,
+		});
+		ticket.currentStep = nextStepNumber;
+		const runner = this.options[nextStep.kind];
+		return runner.run(ticket, text);
+	}
+
+	private validateText(text: string): string {
+		if (!text) {
+			throw {
+				statusCode: 400,
+				message: "Invalid response",
+			};
+		}
+		return text.split(" ").shift();
+	}
+}
