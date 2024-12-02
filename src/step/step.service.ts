b7d73ec504130082af88c68efdf666779a4639e5
Robert Cunha <robert.cunha@techgrowth-labs.com.br>
2024-12-02 16:30:00 -0300

    refactor: remove trash

--- src/step/step.service.ts
+++ src/step/step.service.ts
@@ -5,51 +5,13 @@ import {
 	UserMessageParams,
 } from "../chatbot/chatbot";
 import { Model } from "mongoose";
+import { Step } from "./step.entity";
 
 export abstract class StepService {
-	constructor(protected readonly ticketModel: Model<Ticket>) {}
+	constructor(
+		protected readonly model: Model<Step>,
+		protected readonly ticketModel: Model<Ticket>
+	) {}
 
-	abstract run(
-		ticket: Ticket,
-		params?: UserMessageParams
-	): Promise<OutputTaskInterpretation>;
+	abstract run(ticket: Ticket, text: string): Promise<OutputTaskInterpretation>;
 }
-
-// @Injectable()
-// export class CheckoutStep extends StepService {
-// 	constructor(
-// 		@Inject("StepModel") model: Model<Step>,
-// 		@Inject("Chatbot") chatbot: ChatBot,
-// 		@Inject("Channel") channel: Channel,
-// 		@Inject("TicketModel") ticketModel: Model<Ticket>
-// 	) {
-// 		super(model, chatbot, channel, ticketModel);
-// 	}
-
-// 	async run(ticket: Ticket, contentFromWpp: string): Promise<void> {
-// 		const step = await this.findStep(ticket.currentStep);
-
-// 		const { statusCode, message } = await this.sendMessageBasedOnStep(
-// 			ticket,
-// 			contentFromWpp,
-// 			step.rule
-// 		);
-
-// 		const cartValue = ticket.cart.reduce((acc, product) => {
-// 			return acc + product.price;
-// 		}, 0);
-
-// 		const cartText = ticket.cart
-// 			.map((product) => {
-// 				`${product.name}\nR$: ${product.price.toFixed(2)}`;
-// 			})
-// 			.join("\n");
-
-// 		const checkoutText = `${cartText}\nValor total R$ ${cartValue.toFixed(2)}`;
-
-// 		await this.channel.sendMessage(
-// 			ticket.from,
-// 			`${step.texts[statusCode.toString() as "200" | "400"]}\n${checkoutText}`
-// 		);
-// 	}
-// }
