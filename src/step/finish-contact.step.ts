32abe04ecc5fa49fddc2aecb6d910f1578b2ded9
Robert Cunha <robert.cunha@techgrowth-labs.com.br>
2024-12-04 15:37:58 -0300

    feat: finish contact

---
+++ src/step/finish-contact.step.ts
@@ -0,0 +1,40 @@
+import { Model } from "mongoose";
+import { OutputTaskInterpretation } from "../chatbot/chatbot";
+import { Inject } from "../common/dependency-injection/inject";
+import { Injectable } from "../common/dependency-injection/injectable";
+import { StatusTicket, Ticket } from "../ticket/ticket.entity";
+import { Step } from "./step.entity";
+import { StepService } from "./step.service";
+
+@Injectable()
+export class FinishContactStep extends StepService {
+	constructor(
+		@Inject("StepModel") stepModel: Model<Step>,
+		@Inject("TicketModel") ticketModel: Model<Ticket>
+	) {
+		super(stepModel, ticketModel);
+	}
+
+	override async run(
+		ticket: Ticket,
+		_text: string
+	): Promise<OutputTaskInterpretation> {
+		const step = await this.model
+			.findOne({
+				stepNumber: ticket.currentStep,
+			})
+			.lean();
+		await this.ticketModel.findOneAndUpdate(
+			{
+				_id: ticket._id,
+			},
+			{
+				status: StatusTicket.Closed,
+			}
+		);
+		return {
+			params: {},
+			rule: step.rule,
+		};
+	}
+}
