9bd2383cea7ad3626bade76e083a4e91e45733f4
Robert Cunha <robert.cunha@techgrowth-labs.com.br>
2024-12-07 21:25:27 -0300

    feat: company

---
+++ src/company/company.entity.ts
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import { Prop } from "../common/database/prop.decorator";
+import { Schema } from "../common/database/schema.decorator";
+import { SchemaFactory } from "../common/database/schema.factory";
+
+@Schema({
+	timestamps: true,
+})
+export class Company {
+	@Prop({
+		type: mongoose.Types.ObjectId,
+		default: () => new mongoose.Types.ObjectId(),
+	})
+	_id?: mongoose.Types.ObjectId;
+
+	@Prop({
+		type: String,
+		required: true,
+	})
+	name: string;
+
+	@Prop({
+		type: String,
+		required: true,
+	})
+	nationalId: string;
+
+	@Prop({
+		type: String,
+		required: true,
+	})
+	email: string;
+
+	@Prop({
+		type: String,
+		default: null,
+	})
+	password?: string;
+
+	@Prop({
+		type: String,
+		default: null,
+	})
+	activationCode?: string;
+}
+
+export const CompanySchema = SchemaFactory.createFromClass(Company);
