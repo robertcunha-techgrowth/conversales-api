c0371f6726d5d682a3d21d8e383e7e08cad76332
Robert Cunha <robert.cunha@techgrowth-labs.com.br>
2024-12-14 23:51:36 -0300

    refactor: wait payment

---
+++ src/step/payment/domain/wait-payment.step.ts
@@ -0,0 +1,30 @@
+import { Model } from "mongoose";
+import { OutputTaskInterpretation } from "../../../chatbot/chatbot";
+import { Inject } from "../../../common/dependency-injection/inject";
+import { Injectable } from "../../../common/dependency-injection/injectable";
+import { Ticket } from "../../../ticket/ticket.entity";
+import { StepService } from "../../step.service";
+import { Step } from "../../step.entity";
+
+@Injectable()
+export class WaitPaymentStep extends StepService {
+	constructor(
+		@Inject("StepModel") stepModel: Model<Step>,
+		@Inject("TicketModel") ticketModel: Model<Ticket>
+	) {
+		super(stepModel, ticketModel);
+	}
+
+	override async run(
+		ticket: Ticket,
+		_text: string
+	): Promise<OutputTaskInterpretation> {
+		const step = await this.model.findOne({
+			stepNumber: ticket.currentStep,
+		});
+		return {
+			rule: step.rule,
+			params: {},
+		};
+	}
+}
