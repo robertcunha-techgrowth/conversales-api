fbca6588bb344606f3bf80f43066b9b3b36151d4
Robert Cunha <robert.cunha@techgrowth-labs.com.br>
2024-12-14 23:50:47 -0300

    refactor: step

--- src/step/add-product-step.ts
+++ src/step/add-product-step.ts
@@ -2,7 +2,7 @@ import { Model } from "mongoose";
 import { InputStepParams, UserMessageParams } from "../chatbot/chatbot";
 import { Injectable } from "../common/dependency-injection/injectable";
 import { Ticket } from "../ticket/ticket.entity";
-import { Step, StepKind } from "./step.entity";
+import { Step } from "./step.entity";
 import { StepService } from "./step.service";
 import { Inject } from "../common/dependency-injection/inject";
 import { Product } from "../product/product.entity";
